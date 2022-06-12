9c0918055d457e4cefdd05cd336cda172a14fe18
alperk1 <alpergenel@gmail.com>
2022-06-12 20:45:04 +0300

    added get_quote function

---
+++ tests/test_get_quote.py
@@ -0,0 +1,26 @@
+from random_quote_generator import get_quote, topla
+from random_quote_generator.quotes import quotes
+
+
+def test_get_quote():
+    """
+    GIVEN
+    WHEN get_quote is called
+    THEN random quote from quotes is returned
+    """
+
+    quote = get_quote()
+
+    assert quote in quotes
+
+
+def test_topla():
+    x = 10
+    y = 15
+    toplam = topla(x, y)
+    assert toplam == 150
+
+    x = 10
+    y = -5
+    toplam = topla(x, y)
+    assert toplam == -50
